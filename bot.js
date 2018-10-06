ae5287cb75566902dff6f7c36e3fb0d1bdc041a7
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2018-10-06 22:20:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ bot.on('message', message => {
     var msg = message.content;
     var prefix = "$" //The text before commands, you can put anything that you prefer
 
-    if (message.author.id != '379670051311058944' && message.channel.id === '379669526935240704') {
+    if (message.author.id != '498173512628961280' && message.channel.id === '498174630876545044') {
         if (msg.startsWith('$', 0)) {
             switch (msg) {
                 case "$showProgress":
